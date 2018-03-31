b2d64dfa02390ad0e07a693839a5b2ce33cffc4b
Zubair Ahmed <zubair.ahmed@alumni.stanford.edu>
2018-03-30 17:10:41 -0700

    can parse semvar string into SemVar data structure

--- SemVar.js
+++ SemVar.js
@@ -5,10 +5,26 @@ class SemVar {
         this.major = major
         this.minor = minor
         this.patch = patch
-        // Assume user is adding a hyphen to start preRelease as per spec
+        // Assume user is adding a '-' to start preRelease as per spec
         this.preRelease = preRelease
         // Assume user is adding a '+' to start metadata as per spec
         this.metadata = metadata
+    }
+
+    // 'string described in the specification' from technical challenge implies valid semvar string
+    // so no validity checks are performed
+    static parse(semvarString) {
+        const majorMinorPatchAndPreReleaseMetadataArray = semvarString.split('-', 2)
+        const majorMinorPatchString = majorMinorPatchAndPreReleaseMetadataArray[0]
+        const preReleaseMetadataString = majorMinorPatchAndPreReleaseMetadataArray[1]
+
+        const preReleaseAndMetadataArray = preReleaseMetadataString.split('+')
+        const preReleaseString = preReleaseAndMetadataArray[0]
+        const metadataString = preReleaseAndMetadataArray[1]
+
+        const [majorString, minorString, patchString] = majorMinorPatchString.split('.')
+
+        return new SemVar(majorString, minorString, patchString, '-' + preReleaseString, '+' + metadataString)
     }
 
     getMajor() {
--- test/SemVar.test.js
+++ test/SemVar.test.js
@@ -13,4 +13,14 @@ describe('SemVar class', () => {
         expect(semvar.getMetadata()).to.equal('+001')
     })
 
+    it('can parse semvar string in SemVar data type', () => {
+        const semvarString = '1.2.3-alpha+001'
+        const semvar = SemVar.parse(semvarString)
+
+        expect(semvar.getMajor()).to.equal("1");
+        expect(semvar.getMinor()).to.equal("2");
+        expect(semvar.getPatch()).to.equal("3");
+        expect(semvar.getPreRelease()).to.equal("-alpha");
+        expect(semvar.getMetadata()).to.equal("+001");
+    })
 })
